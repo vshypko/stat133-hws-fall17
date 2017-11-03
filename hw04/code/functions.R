207dd8ad5674a826096d25d8f7958eb53d546e77
Vitali Shypko <vshypko@berkeley.edu>
2017-11-03 14:25:36 -0700

    hw04: start

---
+++ hw04/code/functions.R
@@ -0,0 +1,75 @@
+remove_missing <- function() {
+  
+}
+
+get_minimum <- function() {
+  
+}
+
+get_maximum <- function() {
+  
+}
+
+get_range <- function() {
+  
+}
+
+get_percentile10 <- function() {
+  
+}
+
+get_percentile90 <- function() {
+  
+}
+
+get_quartile1 <- function() {
+  
+}
+
+get_quartile3 <- function() {
+  
+}
+
+get_median <- function() {
+  
+}
+
+get_average <- function() {
+  
+}
+
+get_stdev <- function() {
+  
+}
+
+count_missing <- function() {
+  
+}
+
+summary_stats <- function() {
+  
+}
+
+print_stats <- function() {
+  
+}
+
+drop_lowest <- function() {
+  
+}
+
+rescale100 <- function() {
+  
+}
+
+score_homework <- function() {
+  
+}
+
+score_quiz <- function() {
+  
+}
+
+score_lab <- function() {
+  
+}
