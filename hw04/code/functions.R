89bdb702ea775fac8c3dddfdc37b54b7e90d0fe9
Vitali Shypko <vshypko@berkeley.edu>
2017-11-16 21:09:35 -0800

    hw04: complete functions

--- hw04/code/functions.R
+++ hw04/code/functions.R
@@ -1,75 +1,213 @@
-remove_missing <- function() {
-  
+remove_missing <- function(v) {
+  return(v[!is.na(v)])
 }
 
-get_minimum <- function() {
-  
+get_minimum <- function(v, na.rm = FALSE) {
+  if (!is.numeric(v)) {
+    stop("Non-numeric argument provided")
+  }
+  if (na.rm == TRUE) {
+    v <- remove_missing(v)
+  }
+  v <- sort(x)
+  return(v[1])
 }
 
-get_maximum <- function() {
-  
+get_maximum <- function(v, na.rm = FALSE) {
+  if (!is.numeric(v)) {
+    stop("Non-numeric argument provided")
+  }
+  if (na.rm == TRUE) {
+    v <- remove_missing(v)
+  }
+  v <- sort(x, decreasing = TRUE)
+  return(v[1])
 }
 
-get_range <- function() {
-  
+get_range <- function(v, na.rm = FALSE) {
+  if (!is.numeric(v)) {
+    stop("Non-numeric argument provided")
+  }
+  if (na.rm == TRUE) {
+    v <- remove_missing(v)
+  }
+  return(get_maximum(v) - get_minimum(v))
 }
 
-get_percentile10 <- function() {
-  
+get_median <- function(v, na.rm = FALSE) {
+  if (!is.numeric(v)) {
+    stop("Non-numeric argument provided")
+  }
+  if (na.rm == TRUE) {
+    v <- remove_missing(v)
+  }
+  v <- sort(v)
+  if (length(v) %% 2 == 1) {
+    return(v[(length(v) / 2) + 1])
+  } else {
+    return((v[(length(v) / 2)] + v[(length(v) / 2) + 1]) / 2)
+  }
 }
 
-get_percentile90 <- function() {
-  
+get_average <- function(v, na.rm = FALSE) {
+  if (!is.numeric(v)) {
+    stop("Non-numeric argument provided")
+  }
+  if (na.rm == TRUE) {
+    v <- remove_missing(v)
+  }
+  sum = 0
+  for(i in 1:length(v)) {
+    sum = sum + 1
+  }
+  return(sum / length(v))
 }
 
-get_quartile1 <- function() {
-  
+get_stdev <- function(v, na.rm = FALSE) {
+  if (!is.numeric(v)) {
+    stop("Non-numeric argument provided")
+  }
+  if (na.rm == TRUE) {
+    v <- remove_missing(v)
+  }
+  current_avg = 0
+  sum = 0
+  avg_sum = 0
+  for (i in 1:length(v)) {
+    avg_sum = avg_sum + i
+    current_avg = get_average(v[1:i])
+    sum = sum + (i - current_avg)^2
+  }
+  return(sqrt((1 / (n - 1)) * sum))
 }
 
-get_quartile3 <- function() {
-  
+get_quartile1 <- function(v, na.rm = FALSE) {
+  if (!is.numeric(v)) {
+    stop("Non-numeric argument provided")
+  }
+  if (na.rm == TRUE) {
+    v <- remove_missing(v)
+  }
+  return(quantile(v)[[2]])
+  #???
 }
 
-get_median <- function() {
-  
+get_quartile3 <- function(v, na.rm = FALSE) {
+  if (!is.numeric(v)) {
+    stop("Non-numeric argument provided")
+  }
+  if (na.rm == TRUE) {
+    v <- remove_missing(v)
+  }
+  return(quantile(v)[[4]])
+  #???
 }
 
-get_average <- function() {
-  
+count_missing <- function(v) {
+  if(!is.numeric(v)) {
+    stop("Non-numeric argument provided")
+  }
+  return(sum(v[is.na(v)]))
 }
 
-get_stdev <- function() {
+summary_stats <- function(v) {
+  if(!is.numeric(v)) {
+    stop("Non-numeric argument provided")
+  }
+  minimum = get_minimum(v)
+  percent10 = get_percentile10(v)
+  quartile1 = get_quartile1(v)
+  median = get_median(v)
+  mean = get_average(v)
+  quartile3 = get_quartile3(v)
+  percent90 = get_percentile90(v)
+  maximum = get_maximum(v)
+  range = get_range(v)
+  stdev = get_stdev(v)
+  missing = count_missing(v)
   
+  return(as.list(c(minimum, percent10, quartile1, median, mean, quartile3,
+                   percent90, maximum, range, stdev, missing)))
 }
 
-count_missing <- function() {
-  
+print_stats <- function(v) {
+  print(summary_stats(v))
+  #???
 }
 
-summary_stats <- function() {
-  
+rescale100 <- function(x, xmin, xmax) {
+  if(!is.numeric(x)) {
+    stop("Non-numeric argument provided")
+  }
+  return(100 * (x - xmin) / (xmax - xmin))
 }
 
-print_stats <- function() {
-  
+drop_lowest <- function(n) {
+  if(!is.numeric(n)) {
+    stop("Non-numeric argument provided")
+  }
+  sorted_n <- sort(n)
+  lowest = sorted_n[1]
+  counter = 0
+  for (i in 1:length(n)) {
+    if (n[i] == lowest) {
+      return(n[-i])
+    }
+  }
 }
 
-drop_lowest <- function() {
-  
+score_homework <- function(v, drop = FALSE) {
+  if(!is.numeric(v)) {
+    stop("Non-numeric argument provided")
+  }
+  if (drop == TRUE) {
+    v <- drop_lowest(v)
+  }
+  return(get_average(v))
 }
 
-rescale100 <- function() {
-  
+score_quiz <- function(v, drop = FALSE) {
+  if(!is.numeric(v)) {
+    stop("Non-numeric argument provided")
+  }
+  if (drop == TRUE) {
+    v <- drop_lowest(v)
+  }
+  return(get_average(v))
 }
 
-score_homework <- function() {
-  
+score_lab <- function(v) {
+  if(!is.numeric(v) | v < 0 | v > 12) {
+    stop("Non-numeric argument provided")
+  }
+  switch(
+    v,
+    11 | 12 = return(100),
+    10 = return(80),
+    9 = return(60),
+    8 = return(40),
+    7 = return(20),
+    6 = return(0),
+    score = return(0)
+  )
 }
 
-score_quiz <- function() {
-  
+get_percentile10 <- function(v, na.rm = FALSE) {
+  if(!is.numeric(v)) {
+    stop("Non-numeric argument provided")
+  }
+  if (na.rm == TRUE) {
+    v <- remove_missing(v)
+  }
+  return(quantile(v, .10)[[1]])
 }
 
-score_lab <- function() {
-  
+get_percentile90 <- function(v, na.rm = FALSE) {
+  if (!is.numeric(v)) {
+    stop("Non-numeric argument provided")
+  }
+  if (na.rm == TRUE) {
+    v <- remove_missing(v)
+  }
+  return(quantile(v, .90)[[1]])
 }
