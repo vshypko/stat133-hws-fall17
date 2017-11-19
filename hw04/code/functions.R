afca2daac27387f5c27d0536e74c0cc1a9b0a218
Vitali Shypko <vshypko@berkeley.edu>
2017-11-19 11:46:00 -0800

    hw04: parts 1-3

--- hw04/code/functions.R
+++ hw04/code/functions.R
@@ -1,43 +1,49 @@
+# remove_missing takes a numeric vector, removes missing values from it,
+# and returns the resulting vector.
 remove_missing <- function(v) {
   return(v[!is.na(v)])
 }
 
+# get_minimum takes a numeric vector and an optional na.rm argument. It returns
+# the minimum value of the original input vector if na.rm is FALSE or of
+# the vector with no missing values if na.rm is TRUE.
 get_minimum <- function(v, na.rm = FALSE) {
-  if (!is.numeric(v)) {
-    stop("Non-numeric argument provided")
-  }
+  check_if_numeric(v)
   if (na.rm == TRUE) {
     v <- remove_missing(v)
   }
-  v <- sort(x)
+  v <- sort(v)
   return(v[1])
 }
 
+# get_maximum takes a numeric vector and an optional na.rm argument. It returns
+# the maximum value of the original input vector if na.rm is FALSE or of
+# the vector with no missing values if na.rm is TRUE.
 get_maximum <- function(v, na.rm = FALSE) {
-  if (!is.numeric(v)) {
-    stop("Non-numeric argument provided")
-  }
+  check_if_numeric(v)
   if (na.rm == TRUE) {
     v <- remove_missing(v)
   }
-  v <- sort(x, decreasing = TRUE)
+  v <- sort(v, decreasing = TRUE)
   return(v[1])
 }
 
+# get_range takes a numeric vector and an optional na.rm argument. It returns
+# the overall range (maximum - minimum) of the original input vector if na.rm
+# is FALSE or of the vector with no missing values if na.rm is TRUE.
 get_range <- function(v, na.rm = FALSE) {
-  if (!is.numeric(v)) {
-    stop("Non-numeric argument provided")
-  }
+  check_if_numeric(v)
   if (na.rm == TRUE) {
     v <- remove_missing(v)
   }
   return(get_maximum(v) - get_minimum(v))
 }
 
+# get_median takes a numeric vector and an optional na.rm argument. It returns
+# the median of the original input vector if na.rm is FALSE or of the vector
+# with no missing values if na.rm is TRUE.
 get_median <- function(v, na.rm = FALSE) {
-  if (!is.numeric(v)) {
-    stop("Non-numeric argument provided")
-  }
+  check_if_numeric(v)
   if (na.rm == TRUE) {
     v <- remove_missing(v)
   }
@@ -49,165 +55,191 @@ get_median <- function(v, na.rm = FALSE) {
   }
 }
 
+# get_average takes a numeric vector and an optional na.rm argument. It returns
+# the average of the original input vector if na.rm is FALSE or of the vector
+# with no missing values if na.rm is TRUE.
 get_average <- function(v, na.rm = FALSE) {
-  if (!is.numeric(v)) {
-    stop("Non-numeric argument provided")
-  }
+  check_if_numeric(v)
   if (na.rm == TRUE) {
     v <- remove_missing(v)
   }
   sum = 0
   for(i in 1:length(v)) {
-    sum = sum + 1
+    sum = sum + v[i]
   }
   return(sum / length(v))
 }
 
+# get_stdev takes a numeric vector and an optional na.rm argument. It returns
+# the standard deviation of the original input vector if na.rm is FALSE or of
+# the vector with no missing values if na.rm is TRUE.
 get_stdev <- function(v, na.rm = FALSE) {
-  if (!is.numeric(v)) {
-    stop("Non-numeric argument provided")
-  }
+  check_if_numeric(v)
   if (na.rm == TRUE) {
     v <- remove_missing(v)
   }
-  current_avg = 0
   sum = 0
-  avg_sum = 0
   for (i in 1:length(v)) {
-    avg_sum = avg_sum + i
-    current_avg = get_average(v[1:i])
-    sum = sum + (i - current_avg)^2
+    temp = (v[i] - get_average(v))^2
+    sum = sum + temp
   }
-  return(sqrt((1 / (n - 1)) * sum))
+  result = sqrt(1 / ((length(v) - 1)) * sum)
+  return(result)
 }
 
+# get_quartile1 takes a numeric vector and an optional na.rm argument. It
+# returns the first quartile of the original input vector if na.rm is FALSE or
+# of the vector with no missing values if na.rm is TRUE.
 get_quartile1 <- function(v, na.rm = FALSE) {
-  if (!is.numeric(v)) {
-    stop("Non-numeric argument provided")
-  }
+  check_if_numeric(v)
   if (na.rm == TRUE) {
     v <- remove_missing(v)
   }
   return(quantile(v)[[2]])
-  #???
 }
 
+# get_quartile3 takes a numeric vector and an optional na.rm argument. It
+# returns the third quartile of the original input vector if na.rm is FALSE or
+# of the vector with no missing values if na.rm is TRUE.
 get_quartile3 <- function(v, na.rm = FALSE) {
-  if (!is.numeric(v)) {
-    stop("Non-numeric argument provided")
-  }
+  check_if_numeric(v)
   if (na.rm == TRUE) {
     v <- remove_missing(v)
   }
   return(quantile(v)[[4]])
-  #???
 }
 
+# count_missing takes a numeric vector and returns the number of missing values
+# in it.
 count_missing <- function(v) {
-  if(!is.numeric(v)) {
-    stop("Non-numeric argument provided")
+  check_if_numeric(v)
+  missing = 0
+  for (i in 1:length(v)) {
+    if (is.na(v[i])) {
+      missing = missing + 1
+    }
   }
-  return(sum(v[is.na(v)]))
+  return(missing)
 }
 
+# summary_stats takes a numeric vector and returns a list of summary statistics.
 summary_stats <- function(v) {
-  if(!is.numeric(v)) {
-    stop("Non-numeric argument provided")
-  }
-  minimum = get_minimum(v)
-  percent10 = get_percentile10(v)
-  quartile1 = get_quartile1(v)
-  median = get_median(v)
-  mean = get_average(v)
-  quartile3 = get_quartile3(v)
-  percent90 = get_percentile90(v)
-  maximum = get_maximum(v)
-  range = get_range(v)
-  stdev = get_stdev(v)
+  check_if_numeric(v)
   missing = count_missing(v)
-  
-  return(as.list(c(minimum, percent10, quartile1, median, mean, quartile3,
-                   percent90, maximum, range, stdev, missing)))
+  v <- remove_missing(v)
+  summary <- as.list(c(get_minimum(v), get_percentile10(v), get_quartile1(v),
+                       get_median(v), get_average(v), get_quartile3(v),
+                       get_percentile90(v), get_maximum(v), get_range(v),
+                       get_stdev(v), missing))
+  names(summary) <- c("minimum", "percent10", "quartile1", "median", "mean",
+                      "quartile3", "percent90", "maximum", "range", "stdev",
+                      "missing")
+  return(summary)
 }
 
+# print_stats takes a list of summary statistics and prints the values in a
+# nice format.
 print_stats <- function(v) {
-  print(summary_stats(v))
-  #???
+  for (i in 1:length(v)) {
+    #str_pad(names(v[i]), 10, "right")
+    cat(str_pad(names(v[i]), 9, "right"), ":", sprintf("%.4f", (v[[i]])), "\n")
+  }
 }
 
+# rescale100 takes a numeric vector x, a minimum xmin, and a maximum xmax and
+# returns a rescaled vector with a potential scale from 0 to 100.
 rescale100 <- function(x, xmin, xmax) {
-  if(!is.numeric(x)) {
-    stop("Non-numeric argument provided")
-  }
+  check_if_numeric(x)
   return(100 * (x - xmin) / (xmax - xmin))
 }
 
-drop_lowest <- function(n) {
-  if(!is.numeric(n)) {
-    stop("Non-numeric argument provided")
-  }
-  sorted_n <- sort(n)
-  lowest = sorted_n[1]
-  counter = 0
-  for (i in 1:length(n)) {
-    if (n[i] == lowest) {
-      return(n[-i])
+# drop_lowest takes a numeric vector of length n and returns a vector of
+# length n - 1 by dropping the minimum value.
+drop_lowest <- function(v) {
+  check_if_numeric(v)
+  lowest = get_minimum(v)
+  for (i in 1:length(v)) {
+    if (v[i] == lowest) {
+      return(v[-i])
     }
   }
 }
 
+# score_homework takes a numeric vector and an optional argument drop. It
+# returns the average score of the original input vector if drop is FALSE or
+# the average score of the vector with dropped lowest score if drop is TRUE.
 score_homework <- function(v, drop = FALSE) {
-  if(!is.numeric(v)) {
-    stop("Non-numeric argument provided")
-  }
+  check_if_numeric(v)
   if (drop == TRUE) {
     v <- drop_lowest(v)
   }
   return(get_average(v))
 }
 
+# score_quiz takes a numeric vector and an optional argument drop. It
+# returns the average score of the original input vector if drop is FALSE or
+# the average score of the vector with dropped lowest score if drop is TRUE.
 score_quiz <- function(v, drop = FALSE) {
-  if(!is.numeric(v)) {
-    stop("Non-numeric argument provided")
-  }
+  check_if_numeric(v)
   if (drop == TRUE) {
     v <- drop_lowest(v)
   }
   return(get_average(v))
 }
 
+# score_lab takes a numeric value of lab attendance, and returns the lab score.
 score_lab <- function(v) {
-  if(!is.numeric(v) | v < 0 | v > 12) {
-    stop("Non-numeric argument provided")
+  check_if_numeric(v)
+  check_argument_within_range(v, 0, 12)
+  if (v == 7) {
+    return(20)
+  } else if (v == 8) {
+    return(40)
+  } else if (v == 9) {
+    return(60)
+  } else if (v == 10) {
+    return(80)
+  } else if (v == 11 || v == 12) {
+    return(100)
   }
-  switch(
-    v,
-    11 | 12 = return(100),
-    10 = return(80),
-    9 = return(60),
-    8 = return(40),
-    7 = return(20),
-    6 = return(0),
-    score = return(0)
-  )
+  return(0)
 }
 
+# get_percentile10 takes a numeric vector and an optional na.rm argument. It
+# returns the 10 percentile of the original input vector if na.rm is FALSE or
+# of the vector with no missing values if na.rm is TRUE.
 get_percentile10 <- function(v, na.rm = FALSE) {
-  if(!is.numeric(v)) {
-    stop("Non-numeric argument provided")
-  }
+  check_if_numeric(v)
   if (na.rm == TRUE) {
     v <- remove_missing(v)
   }
   return(quantile(v, .10)[[1]])
 }
 
+# get_percentile90 takes a numeric vector and an optional na.rm argument. It
+# returns the 90 percentile of the original input vector if na.rm is FALSE or
+# of the vector with no missing values if na.rm is TRUE.
 get_percentile90 <- function(v, na.rm = FALSE) {
-  if (!is.numeric(v)) {
-    stop("Non-numeric argument provided")
-  }
+  check_if_numeric(v)
   if (na.rm == TRUE) {
     v <- remove_missing(v)
   }
   return(quantile(v, .90)[[1]])
+}
+
+# check_if_numeric takes an argument and checks whether the provided argument
+# is numeric. If it is not -- it stops execution of the program.
+check_if_numeric <- function(v) {
+  if (!(is.vector(v) && is.numeric(v))) {
+    stop("Non-numeric argument provided")
+  }
+}
+
+# check_argument_within_range takes an argument, a minimum value and a maximum
+# value and checks whether the provided argument is within the specified range.
+# If it is not -- it stops execution of the program.
+check_argument_within_range <- function(v, vmin, vmax) {
+  if (v < vmin || v > vmax) {
+    stop("Argument not within ", vmin, "-", vmax, " range provided")
+  }
 }
---
+++ hw04/code/tests.R
@@ -0,0 +1,273 @@
+source("functions.R")
+
+context("Test remove_missing()")
+test_that("remove_missing works correctly", {
+  a1 <- c(1, 4, 7, NA, 10)
+  a2 <- c(1, NA, NA, NA, 3)
+  a3 <- c(1, 2, 3)
+  a4 <- c(NA, NA, NA)
+  b1 <- c(1, 4, 7, 10)
+  b2 <- c(1, 3)
+  b3 <- c(1, 2, 3)
+  
+  expect_equal(remove_missing(a1), b1)
+  expect_equal(remove_missing(a2), b2)
+  expect_equal(remove_missing(a3), b3)
+  expect_that(length(remove_missing(a4)), equals(0))
+})
+
+context("Test get_minimum()")
+test_that("get_minimum works correctly", {
+  str <- c("a", "2", "3")
+  a1 <- c(1, 2, 7, 100)
+  a2 <- c(100, -100)
+  a3 <- c(0, 1, 0, 1, 4)
+  
+  expect_that(get_minimum(str),
+              throws_error("Non-numeric argument provided"))
+  expect_equal(get_minimum(a1), 1)
+  expect_equal(get_minimum(a2), -100)
+  expect_equal(get_minimum(a3), 0)
+})
+
+context("Test get_maximum()")
+test_that("get_maximum works correctly", {
+  str <- c("a", "2", "3")
+  a1 <- c(1, 2, 7, 100)
+  a2 <- c(100, -100)
+  a3 <- c(0, 4, 0, 4, 4)
+  
+  expect_that(get_maximum(str),
+              throws_error("Non-numeric argument provided"))
+  expect_equal(get_maximum(a1), 100)
+  expect_equal(get_maximum(a2), 100)
+  expect_equal(get_maximum(a3), 4)
+})
+
+context("Test get_range()")
+test_that("get_range works correctly", {
+  str <- c("a", "2", "3")
+  a1 <- c(1, 2, 7, 100)
+  a2 <- c(100, -100)
+  a3 <- c(0, 4, 0, 4, 4)
+  
+  expect_that(get_range(str),
+              throws_error("Non-numeric argument provided"))
+  expect_equal(get_range(a1), 99)
+  expect_equal(get_range(a2), 200)
+  expect_equal(get_range(a3), 4)
+})
+
+context("Test get_median()")
+test_that("get_median works correctly", {
+  str <- c("a", "2", "3")
+  a1 <- c(1, 2, 7, 100)
+  a2 <- c(100, -100)
+  a3 <- c(0, 4, 0, 4, 4)
+  
+  expect_that(get_median(str),
+              throws_error("Non-numeric argument provided"))
+  expect_equal(get_median(a1), 4.5)
+  expect_equal(get_median(a2), 0)
+  expect_equal(get_median(a3), 4)
+})
+
+context("Test get_average()")
+test_that("get_average works correctly", {
+  str <- c("a", "2", "3")
+  a1 <- c(1, 2, 7, 100)
+  a2 <- c(100, -100)
+  a3 <- c(0, 4, 0, 4, 4)
+  
+  expect_that(get_average(str),
+              throws_error("Non-numeric argument provided"))
+  expect_equal(get_average(a1), 27.5)
+  expect_equal(get_average(a2), 0)
+  expect_equal(get_average(a3), 2.4)
+})
+
+context("Test get_stdev()")
+test_that("get_stdev works correctly", {
+  str <- c("a", "2", "3")
+  a1 <- c(1, 2, 7, 100)
+  a2 <- c(100, -100)
+  a3 <- c(0, 4, 0, 4, 4)
+  
+  expect_that(get_stdev(str),
+              throws_error("Non-numeric argument provided"))
+  expect_equal(get_stdev(a1), 48.4, tolerance = 0.1)
+  expect_equal(get_stdev(a2), 141.4, tolerance = 0.1)
+  expect_equal(get_stdev(a3), 2.1, tolerance = 0.1)
+})
+
+context("Test get_quartile1()")
+test_that("get_quartile1 works correctly", {
+  str <- c("a", "2", "3")
+  a1 <- c(1, 2, 7, 100)
+  a2 <- c(-100, 100, 200, 300)
+  a3 <- c(0, 4, 0, 4, 4)
+  
+  expect_that(get_quartile1(str),
+              throws_error("Non-numeric argument provided"))
+  expect_equal(get_quartile1(a1), 1.75)
+  expect_equal(get_quartile1(a2), 50)
+  expect_equal(get_quartile1(a3), 0)
+})
+
+context("Test get_quartile3()")
+test_that("get_quartile3 works correctly", {
+  str <- c("a", "2", "3")
+  a1 <- c(1, 2, 7, 100)
+  a2 <- c(-100, 100, 200, 300)
+  a3 <- c(0, 4, 0, 4, 4)
+  
+  expect_that(get_quartile3(str),
+              throws_error("Non-numeric argument provided"))
+  expect_equal(get_quartile3(a1), 30.25)
+  expect_equal(get_quartile3(a2), 225)
+  expect_equal(get_quartile3(a3), 4)
+})
+
+context("Test count_missing()")
+test_that("count_missing works correctly", {
+  str <- c("a", "2", "3")
+  a1 <- c(1, 2, 7, 100)
+  a2 <- c(-100, NA, 200, NA)
+  a3 <- c(NA, 1, NA, NA, NA)
+  
+  expect_that(count_missing(str),
+              throws_error("Non-numeric argument provided"))
+  expect_equal(count_missing(a1), 0)
+  expect_equal(count_missing(a2), 2)
+  expect_equal(count_missing(a3), 4)
+})
+
+context("Test summary_stats()")
+test_that("summary_stats works correctly", {
+  str <- c("a", "2", "3")
+  a1 <- c(1, 4, 7, NA, 10)
+  
+  expect_that(summary_stats(str),
+              throws_error("Non-numeric argument provided"))
+  expect_that(length(summary_stats(a1)), equals(11))
+  expect_equal(summary_stats(a1)[[1]], 1)
+  expect_equal(summary_stats(a1)[[2]], 1.9)
+  expect_equal(summary_stats(a1)[[3]], 3.25)
+  expect_equal(summary_stats(a1)[[4]], 5.5)
+  expect_equal(summary_stats(a1)[[5]], 5.5)
+  expect_equal(summary_stats(a1)[[6]], 7.75)
+  expect_equal(summary_stats(a1)[[7]], 9.1)
+  expect_equal(summary_stats(a1)[[8]], 10)
+  expect_equal(summary_stats(a1)[[9]], 9)
+  expect_equal(summary_stats(a1)[[10]], 3.87, tolerance = 0.1)
+  expect_equal(summary_stats(a1)[[11]], 1)
+})
+
+context("Test rescale100()")
+test_that("rescale100 works correctly", {
+  str <- c("a", "2", "3")
+  a1 <- c(18, 15, 16, 4, 17, 9)
+  a2 <- c(10, 40, 50, 75)
+  a3 <- c(98, 23, 11, 5, 100)
+  
+  expect_that(rescale100(str),
+              throws_error("Non-numeric argument provided"))
+  expect_equal(rescale100(a1, xmin = 0, xmax = 20), c(90, 75, 80, 20, 85, 45))
+  expect_equal(rescale100(a2, xmin = 10, xmax = 50), c(0, 75, 100, 162.5))
+  expect_equal(rescale100(a3, xmin = 0, xmax = 100), c(98, 23, 11, 5, 100))
+})
+
+context("Test drop_lowest()")
+test_that("drop_lowest works correctly", {
+  str <- c("a", "2", "3")
+  a1 <- c(18, 15, 16, 4, 17, 9)
+  a2 <- c(10, 40, 50, 75)
+  a3 <- c(98, 23, 11, 5, 100)
+  
+  expect_that(drop_lowest(str),
+              throws_error("Non-numeric argument provided"))
+  expect_equal(drop_lowest(a1), c(18, 15, 16, 17, 9))
+  expect_equal(drop_lowest(a2), c(40, 50, 75))
+  expect_equal(drop_lowest(a3), c(98, 23, 11, 100))
+})
+
+context("Test score_homework()")
+test_that("score_homework works correctly", {
+  str <- c("a", "2", "3")
+  a1 <- c(100, 80, 30, 70, 75, 85)
+  a2 <- c(10, 40, 50, 75)
+  
+  expect_that(score_homework(str),
+              throws_error("Non-numeric argument provided"))
+  expect_equal(score_homework(a1, drop = TRUE), 82)
+  expect_equal(score_homework(a1, drop = FALSE), 73.3, tolerance = 0.1)
+  expect_equal(score_homework(a2, TRUE), 55)
+})
+
+context("Test score_quiz()")
+test_that("score_quiz works correctly", {
+  str <- c("a", "2", "3")
+  a1 <- c(100, 80, 70, 0)
+  a2 <- c(10, 40, 50, 75)
+  
+  expect_that(score_quiz(str),
+              throws_error("Non-numeric argument provided"))
+  expect_equal(score_quiz(a1, drop = TRUE), 83.3, tolerance = 0.1)
+  expect_equal(score_quiz(a1, drop = FALSE), 62.5)
+  expect_equal(score_quiz(a2, TRUE), 55)
+})
+
+context("Test score_lab()")
+test_that("score_lab works correctly", {
+  str <- "a"
+  num1 <- 13
+  num2 <- -1
+  
+  expect_that(score_lab(str),
+              throws_error("Non-numeric argument provided"))
+  expect_that(score_lab(num1),
+              throws_error("Argument not within 0-12 range provided"))
+  expect_that(score_lab(num2),
+              throws_error("Argument not within 0-12 range provided"))
+  expect_equal(score_lab(0), 0)
+  expect_equal(score_lab(1), 0)
+  expect_equal(score_lab(2), 0)
+  expect_equal(score_lab(3), 0)
+  expect_equal(score_lab(4), 0)
+  expect_equal(score_lab(5), 0)
+  expect_equal(score_lab(6), 0)
+  expect_equal(score_lab(7), 20)
+  expect_equal(score_lab(8), 40)
+  expect_equal(score_lab(9), 60)
+  expect_equal(score_lab(10), 80)
+  expect_equal(score_lab(11), 100)
+  expect_equal(score_lab(12), 100)
+})
+
+context("Test get_percentile10()")
+test_that("get_percentile10 works correctly", {
+  str <- c("a", "2", "3")
+  a1 <- c(1, 2, 3, 4, 5)
+  a2 <- c(200, 12, 52, 6)
+  a3 <- c(1)
+  
+  expect_that(get_percentile10(str),
+              throws_error("Non-numeric argument provided"))
+  expect_equal(get_percentile10(a1), 1.4)
+  expect_equal(get_percentile10(a2), 7.8)
+  expect_equal(get_percentile10(a3), 1)
+})
+
+context("Test get_percentile90()")
+test_that("remove_missing works correctly", {
+  str <- c("a", "2", "3")
+  a1 <- c(1, 2, 3, 4, 5)
+  a2 <- c(200, 12, 52, 6)
+  a3 <- c(1)
+  
+  expect_that(get_percentile90(str),
+              throws_error("Non-numeric argument provided"))
+  expect_equal(get_percentile90(a1), 4.6)
+  expect_equal(get_percentile90(a2), 155.6)
+  expect_equal(get_percentile90(a3), 1)
+})
